d9b2fb2ded9bef69f22b5a4130d9de2051ffe17f
konstantin <krivlenia@targetprocess.com>
2015-02-23 15:31:33 +0300

    add template for react component

---
+++ lib/create/template/component/react/{=instanceName}/views/{=instanceName}.view.jsx
@@ -0,0 +1,13 @@
+define(function (require) {
+    var React = require('libs/react/react-ex');
+    return React.createClass({
+        displayName: '{=name}',
+        render() {
+            return (
+                <div></div>
+            )
+        }
+    });
+});
+
+
